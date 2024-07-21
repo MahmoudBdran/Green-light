910959afd73acf363b7fd73772b49c0c2a4de257
MahmoudBdran <41477881+MahmoudBdran@users.noreply.github.com>
2024-07-21 22:47:10 +0300

    edits on origins

--- src/main/java/com/erp/greenlight/config/WebConfig.java
+++ src/main/java/com/erp/greenlight/config/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
@@ -23,5 +25,19 @@ public class WebConfig {
     @Bean
     public AuditorAware<Admin> auditorAware(){
         return new AuditorAwareImpl();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200", "http://localhost")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
     }
 }
